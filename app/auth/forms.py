c935bfb174acd3c31f8d3f3b25b5d66f2b5dce5b
Fenglei Dou <67573430@qq.com>
2016-08-21 16:19:49 +0800

    Update forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,13 +1,10 @@
+#coding:utf-8
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 from ..models import User
-import sys
 
-reload(sys)
-
-sys.setdefaultencoding(‘utf8′)
 
 class LoginForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64),
