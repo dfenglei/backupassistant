650431c877aa55194965077ffaf96018ab8d4733
dfenglei <67573430@qq.com>
2016-08-23 21:45:31 +0800

    change title

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[ECHO]'
     FLASKY_MAIL_SENDER = 'echo Admin <dfenglei@sina.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
